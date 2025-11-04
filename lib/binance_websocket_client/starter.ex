e7ec3739369dd14e3460a10713964b7a629877c0
Arturo Gonzalez <arturo@arturosolutions.com.au>
2025-11-04 22:24:04 +0400

    Fixed elixir.yml errors

--- lib/binance_websocket_client/starter.ex
+++ lib/binance_websocket_client/starter.ex
@@ -5,53 +5,55 @@ defmodule BinanceWebsocketClient.Starter do
 
   @retry_ms 15_000
 
-  # Public API
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   @impl true
   def init(opts) do
-    state = %{opts: opts, ws_pid: nil}
-    # Attempt to connect immediately after init
+    Logger.info("Starter initializing; will attempt to connect to Binance WebSocket")
+    state = %{opts: opts, client: nil}
     send(self(), :connect)
     {:ok, state}
   end
 
   @impl true
-  def handle_info(:connect, %{ws_pid: nil} = state) do
-    Logger.info("Starter attempting to connect to Binance WebSocket")
+  def handle_info(:connect, state) do
+    Logger.info("Attempting to start BinanceWebsocketClient...")
 
-    case BinanceWebsocketClient.start_link(state.opts) do
+    case safe_start_client(state.opts) do
       {:ok, pid} ->
-        Logger.info("Binance WebSocket started: #{inspect(pid)}")
-        ref = Process.monitor(pid)
-        {:noreply, Map.put(state, :ws_pid, {pid, ref})}
-
-      {:error, %WebSockex.RequestError{code: code} = reason} ->
-        Logger.error("Binance WebSocket start failed (HTTP #{code}). Will retry in #{@retry_ms} ms. Reason: #{inspect(reason)}")
-        Process.send_after(self(), :connect, @retry_ms)
-        {:noreply, state}
+        Logger.info("BinanceWebsocketClient started (pid=#{inspect(pid)})")
+        Process.monitor(pid)
+        {:noreply, %{state | client: pid}}
 
       {:error, reason} ->
-        Logger.error("Binance WebSocket start failed. Will retry in #{@retry_ms} ms. Reason: #{inspect(reason)}")
-        Process.send_after(self(), :connect, @retry_ms)
-        {:noreply, state}
+        Logger.error("Failed to start BinanceWebsocketClient: #{inspect(reason)}. Retrying in #{@retry_ms / 1000}s...")
+        schedule_retry()
+        {:noreply, %{state | client: nil}}
     end
   end
 
-  def handle_info(:connect, state) do
-    # Already connected; ignore extra connect messages
-    {:noreply, state}
+  @impl true
+  def handle_info({:DOWN, _mref, :process, pid, reason}, %{client: pid} = state) do
+    Logger.warning("BinanceWebsocketClient terminated (pid=#{inspect(pid)}): #{inspect(reason)}. Retrying in #{@retry_ms / 1000}s...")
+    schedule_retry()
+    {:noreply, %{state | client: nil}}
   end
 
   @impl true
-  def handle_info({:DOWN, ref, :process, _pid, reason}, %{ws_pid: {pid, ref}} = state) do
-    Logger.warning("Binance WebSocket process #{inspect(pid)} went down: #{inspect(reason)}. Retrying in #{@retry_ms} ms")
-    Process.demonitor(ref, [:flush])
+  def handle_info(_msg, state), do: {:noreply, state}
+
+  defp schedule_retry do
     Process.send_after(self(), :connect, @retry_ms)
-    {:noreply, %{state | ws_pid: nil}}
   end
 
-  def handle_info(_msg, state), do: {:noreply, state}
+  defp safe_start_client(opts) do
+    try do
+      BinanceWebsocketClient.start_link(opts)
+    catch
+      kind, err ->
+        {:error, {kind, err}}
+    end
+  end
 end
